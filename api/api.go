02f44cb4da993f0e74f195cadd716878ca837f22
er1cw00 <wadahana@qq.com>
2026-08-25 12:33:40 +0800

    misc: change http server to nonblocking

--- api/api.go
+++ api/api.go
@@ -1,11 +1,17 @@
 package api
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/er1cw00/claw.go/base/logger"
 	"github.com/gin-gonic/gin"
 )
+
+var server *http.Server = nil
 
 func Start(host string, port int) error {
 	gin.SetMode(gin.ReleaseMode)
@@ -14,5 +20,31 @@ func Start(host string, port int) error {
 
 	addr := fmt.Sprintf("%s:%d", host, port)
 	logger.Debugf("listen: %s", addr)
-	return r.Run(addr)
+
+	server = &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+
+	// ListenAndServe 会一直阻塞，直到调用 srv.Shutdown
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	logger.Debugf("HTTP 服务已成功停止")
+	return nil
+
+	//return r.Run(addr)
+}
+
+func Stop() {
+	logger.Debugf("Stop HTTP Server ...")
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second) // 设置优雅退出的超时时间（例如 5 秒），防止未处理完的请求无限阻塞
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		logger.Errorf("Stop HTTP Server fail, Err: %v", err)
+		return
+	}
+	logger.Debugf("HTTP Server stopped")
 }
