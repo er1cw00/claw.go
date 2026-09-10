b1ad828f67c5f7a8dfd50ebe38f08cf7973be55b
er1cw00 <wadahana@qq.com>
2026-09-10 17:15:31 +0800

    feat: revert to session key

--- core/agent/agent.go
+++ core/agent/agent.go
@@ -179,7 +179,7 @@ func (agent *Agent) processInboundMessage(ctx context.Context, inboundMessage mo
 		agent.printMessageContent("last message", messages[len(messages)-1].Content)
 		req = agent.buildRequest(messages)
 		if agent.llm.Name() == "opencode" {
-			req.SessionID = session.SessionID()
+			req.SessionID = skey //session.SessionID()
 		}
 		if resp, err = agent.llm.Complete(ctx, req); err != nil {
 			logger.Errorf("[Agent] llm complete fail; err: %v", err)
